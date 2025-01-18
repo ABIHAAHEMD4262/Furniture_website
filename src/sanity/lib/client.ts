9b9ccf4794a80e9f7b1a27d05587bb847e47a13b
ABIHAAHMED4262 <abihaahmed413@gmail.com>
2025-01-19 00:11:51 +0500

    Fix errors

--- src/sanity/lib/client.ts
+++ src/sanity/lib/client.ts
@@ -3,8 +3,8 @@ import { createClient } from 'next-sanity'
 import { apiVersion, dataset, projectId } from '../env'
 
 export const client = createClient({
-  projectId,
-  dataset,
-  apiVersion,
+  projectId:'4hrkqh93',
+  dataset:'production',
+  apiVersion:'2023-10-10',
   useCdn: true, // Set to false if statically generating pages, using ISR or tag-based revalidation
 })
