0dad721d0071dc62b23221b859e01696a7e73c53
ABIHAAHMED4262 <abihaahmed413@gmail.com>
2025-01-18 22:10:01 +0500

    Remove Image link

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,6 @@
 import HeroSection from "@/components/Hero";
 import ProductBoxes from "@/components/ProductBoxes";
-import Image from "next/image";
-import Link from "next/link";
+
 
 export default function Home() {
   return (
